0bf698086729ba2914c1caad44d24712101fa7e2
Michael <873510342@qq.com>
2025-06-06 21:59:55 +0800

    feat(model): 添加 MybatisPlus 自动填充功能
    - 在 BaseEntity 中添加了 createTime 和 updateTime 字段的自动填充注解
    - 新增 MybatisMetaObjectHandler 类实现自动填充逻辑
    - 优化了 isDeleted 字段，添加 @TableLogic 注解实现逻辑删除
    - 在 BaseEntity 中添加了 @JsonIgnore 注解，避免序列化时包含敏感字段

---
+++ common/src/main/java/com/lau56/lease/common/mybatisplus/MybatisMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.lau56.lease.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * MybatisPlus 自动填充处理器
+ */
+
+@Component
+public class MybatisMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
